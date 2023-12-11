61941465f028dc3a53c883a191d08641db14d618
Roshan Shah <roshanshah639@gmail.com>
2023-12-11 19:36:29 +0545

    middleware

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,30 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 const Profile = () => {
+  const router = useRouter();
+  const [data, setData] = useState("User Details");
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logged out successfully");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
+  const getUserDetails = async () => {
+    const response = await axios.get("/api/users/me");
+    console.log(response.data);
+    setData(response.data.data._id);
+  };
+
   return (
     <div className="bg-cyan-900 text-white max-w-3xl mx-auto p-8 mt-5 rounded-lg shadow-md">
       <div className="flex items-center bg-cyan-900">
@@ -9,13 +33,23 @@ const Profile = () => {
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRb3hRVx8Le1Hss4fCVZTngm0Nrwa1WXFCL2usvyCuH1XRM40kgCvOuihFXiMn4ODJOa9Q&usqp=CAU"
           alt="Profile"
         />
-        <div className="bg-cyan-900">
+        <div className="bg-cyan-900 ">
           <h2 className="text-2xl bg-cyan-900 ">Roshan Shah</h2>
           <p className=" bg-cyan-900">Software Developer</p>
           <p className=" bg-cyan-900">Kathmandu, Nepal</p>
         </div>
       </div>
-
+      <div className="bg-cyan-700 mt-5 mr-5 p-4 rounded-md flex items-center justify-center text-white">
+        <h2 className="bg-transparent">
+          {data === "User Details" ? (
+            "User Details"
+          ) : (
+            <Link className="bg-transparent" href={`/profile/${data}`}>
+              {data}
+            </Link>
+          )}
+        </h2>
+      </div>
       <div className="mt-8 bg-cyan-900">
         <h2 className="text-xl font-bold mb-4 bg-cyan-900">
           Professional Summary
@@ -27,6 +61,18 @@ const Profile = () => {
           responsive and user-friendly interfaces.
         </p>
       </div>
+      <button
+        onClick={logout}
+        className="bg-cyan-600 mt-5 hover:bg-cyan-500 text-white py-2 px-9 rounded-full text-xl"
+      >
+        Logout
+      </button>
+      <button
+        onClick={getUserDetails}
+        className="bg-green-600 mt-5 ml-3 hover:bg-green-500 text-white py-2 px-9 rounded-full text-xl"
+      >
+        Get User Details
+      </button>
     </div>
   );
 };
---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const isPublicPath = path === "/login" || path === "/signup";
+
+  const token = request.cookies.get("token")?.value || "";
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+}
+
+export const config = {
+  matcher: ["/", "/login", "/signup", "/profile"],
+};
